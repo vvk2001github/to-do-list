51f3e50e15cb34ca4acd62e513df3befa20c6831
kulyaginv <kulyaginv@gmail.com>
2025-11-12 14:31:11 +0500

    Tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,24 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
+    use RefreshDatabase;
+
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $response = $this->getJson('/tasks');
 
-        $response->assertStatus(200);
+        $response->assertOk();
+    }
+
+    public function test_the_application_returns_a_failed_response(): void
+    {
+        $response = $this->postJson('/tasks');
+
+        $response->assertUnprocessable();
     }
 }
